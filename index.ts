bd8bb1bafed72eb5ed69c14dc83546f41b0ea44f
bureau-de-test <bureau.de.test@gmail.com>
2025-05-31 13:01:48 +0300

    chore...

--- index.ts
+++ index.ts
@@ -1,4 +1,12 @@
 #!/usr/bin/env bun
 
+import Elysia from "elysia";
+
+const filderExists = await Bun.file('.adsa').exists()
+
+console.log(filderExists);
+
 console.log("Hello!");
 console.log(process.argv);
+
+const app = new Elysia()
